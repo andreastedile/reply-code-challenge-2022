328ee0efab7257379b0284f131403fb3fec9e23d
Iron512 <ivanmartini96@gmail.com>
2022-03-10 17:45:13 +0100

    input added

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 import os
 
 
+class Demon:
+    def __init__(self, stamina: int, resttime: int, recovery: int, fragments: list[int]):
+        self.stamina = stamina
+        self.rest_time = resttime
+        self.recovery = recovery
+        self.fragments = fragments
+
+    def __str__(self):
+        return f"Demon requires {self.stamina} (gives {self.recovery} after {self.rest_time} turns) and has {len(self.fragments)} fragments ({self.fragments})"
+
+
+class Pandora:
+    def __init__(self, stamina: int, max_stamina: int, turns: int):
+        self.stamina = stamina
+        self.max_stamina = max_stamina
+        self.turns = turns
+
+    def __str__(self):
+        return f"Pandora has {self.stamina} stamina (out of {self.max_stamina}) and {self.turns} turns"
+
+
 def main(dataset_name: str):
+    print(f"working on {dataset_name}")
+
     with open(dataset_name) as f:
-        lines = f.readlines()
-        lines = [line.removesuffix("\n") for line in lines]
+        firstline = f.readline().split()
+        pandorina = Pandora(int(firstline[0]), int(firstline[1]), int(firstline[2]))
+        demons: list[Demon] = []
+        for i in range(int(firstline[3])):
+            line = f.readline().split()
+            stamina = int(line[0])
+            resttime = int(line[1])
+            recovery = int(line[2])
+            fragments = []
+            if line[3] != 0:
+                fragments: list[int] = [int(x) for x in line[4:-1] + [line[-1]]]
+            demons.append(Demon(stamina, resttime, recovery, fragments))
+
+    print(pandorina)
+    for demon in demons:
+        print(demon)
 
     filename: str = dataset_name.replace("./datasets/", "").replace("txt", "")
-    print(filename)
-    with open("./results/" + filename + "_OUT.txt", "w") as f:
-        f.writelines(lines)
+    # with open("./results/" + filename + "_OUT.txt", "w") as f:
 
 
 if __name__ == "__main__":
     content = os.listdir("./datasets/")
+    content = ["00-example.txt"]
     for file in content:
         main("./datasets/" + file)
